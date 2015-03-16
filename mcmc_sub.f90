85a212bf3d16d1452b23974be05d15bade488d10
Yuchen Wang <ycwang0712@gmail.com>
2015-03-15 19:12:48 -0500

    misc format change

--- mcmc_sub.f90
+++ mcmc_sub.f90
@@ -1,9 +1,8 @@
-subroutine mcmc(l,g0,g1,g,p,result)
+subroutine mcmc(l, g0, g1, g, p, result)
   implicit none
   integer, intent(in)::l, g0, g1, g
-  real, dimension(0:2, l), intent(in) :: p
-
-  integer(kind=8), dimension(l, 0:2),intent(out):: result
+  real(kind = 8), dimension(0:2, l), intent(in) :: p
+  integer, dimension(l, 0:2), intent(out):: result
 
   interface
     function prior(total)
@@ -21,30 +20,27 @@ subroutine mcmc(l,g0,g1,g,p,result)
     end subroutine substitute
   end interface
 
-
-
   ! problem-length vector
   integer, dimension(l) :: Iold, Iprop
 
-
-
   ! iteration-length vector
-  real(kind=8), dimension(0:g) :: logprob
+  real(kind = 8), dimension(0:g) :: logprob
   integer, dimension(l, 0:2) :: sum_counts
 
-  real(kind=8) p1, p2, mh, delta1, delta2
-  real(kind=8), dimension(l) :: lk2
+  real(kind = 8) p1, p2, mh, delta1, delta2
+  real(kind = 8), dimension(l) :: lk2
   logical is_sub
   integer, dimension(0:2) :: total_old, total_prop
   integer, dimension(2) :: pos
   integer :: acpt = 0
 
   ! misc
-  integer :: i
-  real :: t1, t2, t3, t4, t5, t_s1 = 0, t_s2 = 0, t_s3 = 0
+  integer i
+  real rand
+  ! real :: t1, t2, t3, t4, t5, t_s1 = 0, t_s2 = 0, t_s3 = 0
 
-  call cpu_time(t1)
   ! call init_random_seed()
+  ! call cpu_time(t1)
 
   ! set initial Iold
   ! g0, g1, and l-g0-g1 is the initial count for 1, 2, 0
@@ -52,6 +48,10 @@ subroutine mcmc(l,g0,g1,g,p,result)
   Iold((g0+1):(g0+g1)) = 2
   Iold((g0+g1+1):l) = 0
   total_old = (/l-g0-g1, g0, g1/)
+
+  ! print *, Iold
+  ! print *, total_old
+
   where (Iold == 0)
     lk2 = p(0, :)
   else where (Iold == 1)
@@ -61,21 +61,24 @@ subroutine mcmc(l,g0,g1,g,p,result)
   end where
   logprob(0) = sum(lk2) + prior(total_old)
 
-  sum_counts=0
+  ! print *, lk2
+  ! print *, logprob
+
+  sum_counts = 0
   do i = 1, g
-    call cpu_time(t2)
+    ! call cpu_time(t2)
 
     call random_number(rand)
     is_sub = rand < .5
     call substitute(Iold, Iprop, l, is_sub, pos)
 
     ! if (modulo(i, l*30) == 0) then
-    !   print *, total
+    !   print *, total_old
     !   print *, i
     ! end if
 
-    call cpu_time(t3)
-    t_s1 = t_s1 + t3 - t2
+    ! call cpu_time(t3)
+    ! t_s1 = t_s1 + t3 - t2
 
 
     total_prop = total_old
@@ -99,8 +102,8 @@ subroutine mcmc(l,g0,g1,g,p,result)
     end if
     mh = delta1 + delta2 + log(p2) - log(p1)
 
-    call cpu_time(t4)
-    t_s2 = t_s2 + t4 - t3
+    ! call cpu_time(t4)
+    ! t_s2 = t_s2 + t4 - t3
 
     ! if accepted, Iold = Iprop
     ! if not, Iold doesn't change
@@ -117,6 +120,7 @@ subroutine mcmc(l,g0,g1,g,p,result)
       if (rand < exp(mh)) then
         acpt = acpt + 1
       end if
+
       where (Iold == 0)
         sum_counts(:, 0) = sum_counts(:, 0) + 1
       elsewhere (Iold == 1)
@@ -124,12 +128,15 @@ subroutine mcmc(l,g0,g1,g,p,result)
       elsewhere(Iold == 2)
         sum_counts(:, 2) = sum_counts(:, 2) + 1
       end where
+
     end if
 
-    call cpu_time(t5)
-    t_s3 = t_s3 + t5 - t4
+    ! call cpu_time(t5)
+    ! t_s3 = t_s3 + t5 - t4
   end do
-  result= sum_counts
+  result = sum_counts
+
+
   !open(10, file='result_Iprop.txt')
   !write(10, *), maxloc(sum_counts, dim=2) - 1
   !close(10)
@@ -142,8 +149,6 @@ subroutine mcmc(l,g0,g1,g,p,result)
   !write(13, *), logprob
   !close(13)
 
-
-
   !print '(f8.4, a)', real(acpt)/l/2, "%"
 
   !call cpu_time(t5)
@@ -151,9 +156,6 @@ subroutine mcmc(l,g0,g1,g,p,result)
   !print *, 'total time taken', (t5-t1)
 
 end subroutine mcmc
-
-
-
 
 
 subroutine substitute(Iold, Iprop, l, sub_flag, pos)
