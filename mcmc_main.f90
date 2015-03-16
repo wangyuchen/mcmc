a53e1f681f50598a3affc80816d4027621fd30f1
Yuchen Wang <ycwang0712@gmail.com>
2015-03-15 19:09:21 -0500

    change precision
    use double precision for p and all related variables.
    use integer for others

--- mcmc_main.f90
+++ mcmc_main.f90
@@ -1,15 +1,15 @@
 program main
   integer, parameter:: l=2000, g0=100, g1=200, g=l*1000
-  real, dimension(0:2, l)::p
-  integer(kind=8), dimension(l, 0:2):: result
+  real(kind = 8), dimension(0:2, l)::p
+  integer, dimension(l, 0:2):: result
   integer, dimension(l):: Iprop
   external mcmc
 
   open(1, file='p_matrix.txt', status='old', action='read')
   read(1,*) p
-  print*,p(0:2, 1)
-  print*,p(0:2, 2)
-  print*, g
+  ! print*,p(0:2, 1)
+  ! print*,p(0:2, 2)
+  ! print*, g
 
   call mcmc(l,g0, g1, g, p, result)
   Iprop=maxloc(result, dim=2) - 1
